c2e97417d32e086b3b84e556d450805f11c8acf6
Hakan Cansever <cansever.hakan94@gmail.com>
2023-10-19 09:53:41 +0200

    timer

--- script.js
+++ script.js
@@ -54,6 +54,7 @@ const labelSumIn = document.querySelector('.summary__value--in');
 const labelSumOut = document.querySelector('.summary__value--out');
 const labelSumInterest = document.querySelector('.summary__value--interest');
 const labelTimer = document.querySelector('.timer');
+const body = document.querySelector('body')
 
 const containerApp = document.querySelector('.app');
 const containerMovements = document.querySelector('.movements');
